3052680aa5fe6464914ed22a87dfd471ab59ac5b
JEONG.MINKYO@nbt.com <jeong.minkyo@nbt.com>
2018-02-07 17:23:27 +0900

    잔액 표시 제거, check box 잘못 선택시 경고문구 수

--- app/controllers/receive_credits_controller.rb
+++ app/controllers/receive_credits_controller.rb
@@ -34,10 +34,15 @@ class ReceiveCreditsController < ApplicationController
           pay = Credit.find_by(id: index)
           pay.update(status: nil)
         end
+
+        respond_to do |format|
+          format.html { redirect_to recent_credits_url, notice: '복구가 성공적으로 이루어졌습니다.' }
+        end
       end
-    end
+    else
     respond_to do |format|
-      format.html { redirect_to recent_credits_url, notice: 'Credit was successfully created.' }
+      format.html { redirect_to recent_credits_url, notice: '복구가 목록을 선택하세요.' }
+      end
     end
   end
 
@@ -51,11 +56,17 @@ class ReceiveCreditsController < ApplicationController
           pay = Credit.find_by(id: index)
           pay.update(status: 1)
         end
+
+        respond_to do |format|
+          format.html { redirect_to receive_credits_url, notice: '수금이 정상적으로 이루어졌습니다.' }
+        end
+      end
+    else
+    respond_to do |format|
+      format.html { redirect_to receive_credits_url, notice: '수금할 목록을 선택하세요.' }
       end
     end
-    respond_to do |format|
-      format.html { redirect_to receive_credits_url, notice: 'Credit was successfully created.' }
-    end
   end
+
 end
 
