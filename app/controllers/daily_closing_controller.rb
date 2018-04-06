82fd6ab215337e6b6ef7e2074247d0a6d166ff2b
JEONG.MINKYO@nbt.com <jeong.minkyo@nbt.com>
2018-04-06 18:41:40 +0900

    일일마감 장부 로직을 변경하다.

--- app/controllers/daily_closing_controller.rb
+++ app/controllers/daily_closing_controller.rb
@@ -4,11 +4,13 @@ class DailyClosingController < ApplicationController
     Delivary_ready = 0
     Delivary_done = 1
     Delivary_checking = 2
+    Delivary_credit = 3
   end
 
   def closing
     @delivary = Delivary.where('deliver = ? and status = ?', params[:deliver], 0)
     @check_delivary = Delivary.where('deliver = ? and status = ?', params[:deliver], 2)
+    @credit_delivary = Delivary.where('deliver = ? and status = ?', params[:deliver], 3)
 
     if params[:daily_closing_date].nil?
 
@@ -37,7 +39,7 @@ class DailyClosingController < ApplicationController
       product_name = params[:product_name]
       product_num = params[:product_num].to_i
       deliver = params[:deliver]
-      status = Status::Delivary_checking
+      status = Status::Delivary_ready
 
       @add_delivary = Delivary.new(:date => date, :name => name, :product_name => product_name, :product_num => product_num, :deliver => deliver, :status => status)
 
@@ -51,21 +53,27 @@ class DailyClosingController < ApplicationController
     end
   end
 
-  def update_credit
+  def add_credit
     if params[:credits_ids].present?
       credits_ids = params[:credits_ids]
       credits_ids.each do |credit|
         index = credit.to_i
         delivary = Delivary.find_by(id: index)
-        date = delivry.date
+        date = delivary.date
         name = delivary.name
-        product_name = 'gas_' + delivary.product_name.to_s
-        Config.first.product_name.to_s
+        cost = Config.where('product_name = ?',delivary.product_name).first.cost * delivary.product_num
+        status = nil
+        product_name = delivary.product_name
+        product_num = delivary.product_num
+        @add_credit = Credit.new(:date => date, :name => name, :product_name => product_name, :product_num => product_num, :cost => cost, :status => status)
+        delivary.update(status: Status::Delivary_credit)
       end
     end
 
     respond_to do |format|
-      format.html { redirect_to daily_closing_path(:deliver => params[:deliver] ) }
+      if @add_credit.save
+        format.html { redirect_to daily_closing_path(:deliver => params[:deliver] ) }
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     get '/daily_closing' => :closing
     post '/daily_closing' => :update_delivary
     post '/daily_closing/add_delivary' => :add_delivary
+    post '/daily_closing/add_credit' => :add_credit
   end
   controller :config do
     get '/configs' => :index
