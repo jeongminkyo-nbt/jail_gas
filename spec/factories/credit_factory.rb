e2467d6394556c27fae256b1e71f07670c93d15c
JEONG.MINKYO@nbt.com <jeong.minkyo@nbt.com>
2018-05-04 15:29:03 +0900

    rubocop 설정

--- spec/factories/credit_factory.rb
+++ spec/factories/credit_factory.rb
@@ -4,7 +4,7 @@ class CreditFactory
       date '2018-04-03'
       name { Faker::Internet.user_name }
       product_name '50kg'
-      product_num {  Faker::Number.number(10) }
+      product_num {  Faker::Number.number(10).to_s }
       created_at '2010-01-01'
       updated_at '2010-01-01'
     end
--- test/models/delivary_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DelivaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
